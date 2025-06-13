a11ff5d45d85f58bce4819b9ff4df68526290f3b
Xiongfei Wei <isaacwxf23@gmail.com>
2025-06-13 10:01:25 -0700

    migrate the code from gs://vllm-cb-storage/code to github

---
+++ legacy/create_and_upload_image.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+if [ ! -f "$1" ]; then
+  echo "Error: The env file '$1' does not exist."
+  echo "Error: The env file '$1' does not exist." >> $RESULT
+  exit 1  # Exit the script with a non-zero status to indicate an error
+fi
+
+ENV_FILE=$1
+
+set -a
+source $ENV_FILE
+set +a
+
+if [ -d "$HOME/miniconda3" ]; then
+  CONDA="$HOME/miniconda3"
+elif [ -d "/opt/conda" ]; then
+  CONDA="/opt/conda"
+else
+  echo "Error: Conda installation not found." >&2
+  exit 1
+fi
+
+echo "source $CONDA/bin/activate vllm"
+echo
+source $CONDA/bin/activate vllm
+
+echo "pip install google-cloud-spanner"
+echo
+pip install google-cloud-spanner
+
+echo "pip install pandas"
+echo
+pip install pandas
+
+echo "pip install pytz"
+echo
+pip install pytz
+
+echo "pip install matplotlib"
+echo
+pip install matplotlib
+
+# python draw.py nightly $HOME/daily.png llama-8b qwen-2b
+# echo gsutil cp daily.png* gs://$GCS_BUCKET
+# gsutil cp daily.png* gs://$GCS_BUCKET
+echo "create source and image"
+echo
+
+python draw.py hourly $HOME/hourly.png llama-8b qwen-2b llama-70b llama-70b-vm llama-8b-vm qwen-2b-vm 
+echo gsutil cp hourly.png* gs://$GCS_BUCKET
+gsutil cp hourly.png* gs://$GCS_BUCKET
+echo gsutil cp hourly.png* gs://$GCS_BUCKET2
+gsutil cp hourly.png* gs://$GCS_BUCKET2
---
+++ legacy/upload_spanner.py
@@ -0,0 +1,85 @@
+from google.cloud import spanner
+from datetime import datetime
+import csv
+import pytz
+import sys
+import os
+import socket
+from google.api_core.exceptions import AlreadyExists
+
+# Initialize Spanner client
+spanner_client = spanner.Client(project=os.getenv("PROJECT_ID"))
+instance = spanner_client.instance(os.getenv("SPANNER_INSTANCE"))
+database = instance.database(os.getenv("SPANNER_DB"))
+seattle_tz = pytz.timezone("America/Los_Angeles")
+
+# os.getenv("HOSTNAME") won't work here.
+hostname = socket.gethostname()
+def parse_csv(file_path, run_type, model_name):
+    records = []    
+    with open(file_path, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if len(row) == 0:
+                continue
+            if len(row) != 3:
+                raise ValueError(f"Invalid row format: {row}")  
+            run_time = datetime.strptime(row[0], "%Y%m%d_%H%M%S")
+            run_time = seattle_tz.localize(run_time)
+            code_hash = row[1]
+            throughput = float(row[2]) if row[2] else 0.0
+            # 'RunTime', 'ModelName', 'RunType', 'CodeHash', 'Throughput', 'Tag', 'VM'
+            records.append((run_time, model_name, run_type, code_hash, throughput, row[0], hostname))
+    return records
+
+def insert_to_spanner(records):
+    inserted = 0
+    for record in records:        
+        try:
+            # Directly insert one record at a time without batch
+            with database.batch() as batch:
+                batch.insert(
+                    table='ModelMetrics',
+                    columns=('RunTime', 'ModelName', 'RunType', 'CodeHash', 'Throughput', 'Tag', 'VM'),
+                    values=[record]
+                )
+            inserted += 1
+        except AlreadyExists:  # Catch duplicate key error
+            print(f"Duplicate key error encountered for record: {record}. Skipping.")
+    print(f"{inserted} of {len(records)} inserted.")
+
+
+def process_one_file(file_path, run_type,model_name, start_row):    
+    records = parse_csv(file_path, run_type, model_name)
+    records_to_process = records[start_row:]
+    insert_to_spanner(records_to_process)
+    return len(records)
+
+if __name__ == "__main__":
+    # if len(sys.argv) != 3:
+    #     print("Usage: python upload_spanner.py $HOME/table.txt nightly")
+    #     sys.exit(1)
+    if len(sys.argv) != 2:
+        print("Usage: python upload_spanner.py <state_file>")
+        sys.exit(1)
+
+    config_path = sys.argv[1]    
+    configs = []
+    with open(config_path, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if len(row) != 4:
+                raise ValueError(f"Invalid row format in config: {row}")  
+            # result path, run_type, model_name, start_index
+            configs.append((row[0],row[1], row[2], int(row[3])))
+
+    write_back = configs[:]
+    for ii, config in enumerate(configs):
+        rows = process_one_file(config[0], config[1], config[2], config[3])
+        write_back[ii] = (config[0], config[1], config[2], rows)
+
+
+    with open(config_path, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(write_back)
+    
