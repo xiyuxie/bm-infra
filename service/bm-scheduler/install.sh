2da7e9285aa04e0c8da54d3049c340943d907b43
Qiliang Cui <derrhein@gmail.com>
2025-06-14 17:01:08 +0000

    Add scheduler installer.
    Signed-off-by: Qiliang Cui <derrhein@gmail.com>

--- service/bm-scheduler/install.sh
+++ service/bm-scheduler/install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
-if ! id -u bm-agent >/dev/null 2>&1; then
-    echo "sudo useradd -m -s /bin/bash bm-agent"
-    sudo useradd -m -s /bin/bash bm-agent
+if ! id -u bm-scheduler >/dev/null 2>&1; then
+    echo "sudo useradd -m -s /bin/bash bm-scheduler"
+    sudo useradd -m -s /bin/bash bm-scheduler
 fi
 
-echo "sudo usermod -aG docker bm-agent"
-sudo usermod -aG docker bm-agent
+echo "sudo usermod -aG docker bm-scheduler"
+sudo usermod -aG docker bm-scheduler
 
 echo "sudo apt-get update && sudo apt-get install -y jq"
 sudo apt-get update && sudo apt-get install -y jq
 
-echo "sudo -u bm-agent -i..."
-sudo -u bm-agent -i bash <<EOF
+echo "sudo -u bm-scheduler -i..."
+sudo -u bm-scheduler -i bash <<EOF
 echo "gcloud auth configure-docker $GCP_REGION-docker.pkg.dev --quiet"
 gcloud auth configure-docker $GCP_REGION-docker.pkg.dev --quiet
 
@@ -25,17 +25,17 @@ git clone https://github.com/QiliangCui/bm-infra.git
 
 EOF
 
-echo "sudo cp /home/bm-agent/bm-infra/service/bm-agent/bm-agent.service /etc/systemd/system/bm-agent.service"
-sudo cp /home/bm-agent/bm-infra/service/bm-agent/bm-agent.service /etc/systemd/system/bm-agent.service
+echo "sudo cp /home/bm-scheduler/bm-infra/service/bm-scheduler/bm-scheduler.service /etc/systemd/system/bm-scheduler.service"
+sudo cp /home/bm-scheduler/bm-infra/service/bm-scheduler/bm-scheduler.service /etc/systemd/system/bm-scheduler.service
 
 echo "sudo systemctl daemon-reload"
 sudo systemctl daemon-reload
 
-echo "sudo systemctl stop bm-agent.service"
-sudo systemctl stop bm-agent.service
+echo "sudo systemctl stop bm-scheduler.service"
+sudo systemctl stop bm-scheduler.service
 
-echo "sudo systemctl enable bm-agent.service"
-sudo systemctl enable bm-agent.service
+echo "sudo systemctl enable bm-scheduler.service"
+sudo systemctl enable bm-scheduler.service
 
-echo "sudo systemctl start bm-agent.service"
-sudo systemctl start bm-agent.service
+echo "sudo systemctl start bm-scheduler.service"
+sudo systemctl start bm-scheduler.service
