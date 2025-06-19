9d18412c9a36614d7a05da5fd44b20f97b05b697
Qiliang Cui <derrhein@gmail.com>
2025-06-19 17:52:43 +0000

    Add cleanup in hourly run scheduler.
    Signed-off-by: Qiliang Cui <derrhein@gmail.com>

--- scripts/scheduler/hourly_run.sh
+++ scripts/scheduler/hourly_run.sh
@@ -5,3 +5,6 @@ TAG="$(TZ="$TIMEZONE" date +%Y%m%d_%H%M%S)"
 
 echo "./scripts/scheduler/create_job.sh ./cases/hourly.csv \"\" $TAG HOURLY"
 ./scripts/scheduler/create_job.sh ./cases/hourly.csv "" $TAG HOURLY
+
+echo "./scripts/cleanup_docker.sh"
+./scripts/cleanup_docker.sh
