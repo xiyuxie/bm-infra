9804ff9733d55c57f4a666c12e1298a40f36dfe7
Qiliang Cui <derrhein@gmail.com>
2025-06-20 20:44:03 +0000

    Enable autotuen with hourly.
    Signed-off-by: Qiliang Cui <derrhein@gmail.com>

--- scripts/scheduler/hourly_run.sh
+++ scripts/scheduler/hourly_run.sh
@@ -6,5 +6,8 @@ TAG="$(TZ="$TIMEZONE" date +%Y%m%d_%H%M%S)"
 echo "./scripts/scheduler/create_job.sh ./cases/hourly.csv \"\" $TAG HOURLY"
 ./scripts/scheduler/create_job.sh ./cases/hourly.csv "" $TAG HOURLY
 
+echo "./scripts/scheduler/create_job.sh ./cases/autotune.csv \"\" $TAG AUTOTUNE"
+./scripts/scheduler/create_job.sh ./cases/autotune.csv "" $TAG AUTOTUNE
+
 echo "./scripts/cleanup_docker.sh"
 ./scripts/cleanup_docker.sh
