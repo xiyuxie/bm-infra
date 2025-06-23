4e0c27632fd99b4650cc8a213380fe46794dae2b
Qiliang Cui <derrhein@gmail.com>
2025-06-23 18:25:03 +0000

    Run autotune only at 12:00 in the morning.
    Signed-off-by: Qiliang Cui <derrhein@gmail.com>

--- scripts/scheduler/hourly_run.sh
+++ scripts/scheduler/hourly_run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-
 TIMEZONE="America/Los_Angeles"
 TAG="$(TZ="$TIMEZONE" date +%Y%m%d_%H%M%S)"
+HOUR_NOW=$(TZ="$TIMEZONE" date +%H)
 
 echo "./scripts/scheduler/create_job.sh ./cases/hourly.csv \"\" $TAG HOURLY"
 ./scripts/scheduler/create_job.sh ./cases/hourly.csv "" $TAG HOURLY
 
-echo "./scripts/scheduler/create_job.sh ./cases/autotune.csv \"\" $TAG AUTOTUNE"
-./scripts/scheduler/create_job.sh ./cases/autotune.csv "" $TAG AUTOTUNE
+if [[ "$HOUR_NOW" == "00" ]]; then
+  echo "./scripts/scheduler/create_job.sh ./cases/autotune.csv \"\" $TAG AUTOTUNE"
+  ./scripts/scheduler/create_job.sh ./cases/autotune.csv "" $TAG AUTOTUNE
+fi
 
 echo "./scripts/cleanup_docker.sh"
 ./scripts/cleanup_docker.sh
