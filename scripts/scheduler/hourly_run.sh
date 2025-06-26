ab3af7db881fc452ec06548b6318fc279c8a6b0e
Qiliang Cui <derrhein@gmail.com>
2025-06-26 02:05:18 +0000

    Separate h100 to 2 runs.
    Signed-off-by: Qiliang Cui <derrhein@gmail.com>

--- scripts/scheduler/hourly_run.sh
+++ scripts/scheduler/hourly_run.sh
@@ -6,6 +6,16 @@ HOUR_NOW=$(TZ="$TIMEZONE" date +%H)
 echo "./scripts/scheduler/create_job.sh ./cases/hourly.csv \"\" $TAG HOURLY"
 ./scripts/scheduler/create_job.sh ./cases/hourly.csv "" $TAG HOURLY
 
+# Run gpu_1 on even hours, gpu_2 on odd hours
+# Because I don't have enough h100-8 now.
+if (( 10#$HOUR_NOW % 2 == 0 )); then
+  echo "./scripts/scheduler/create_job.sh ./cases/hourly_gpu_1.csv \"\" $TAG HOURLY"
+  ./scripts/scheduler/create_job.sh ./cases/hourly_gpu_1.csv "" $TAG HOURLY
+else
+  echo "./scripts/scheduler/create_job.sh ./cases/hourly_gpu_2.csv \"\" $TAG HOURLY"
+  ./scripts/scheduler/create_job.sh ./cases/hourly_gpu_2.csv "" $TAG HOURLY
+fi
+
 if [[ "$HOUR_NOW" == "00" || "$HOUR_NOW" == "12" ]]; then
   echo "./scripts/scheduler/create_job.sh ./cases/autotune.csv \"\" $TAG AUTOTUNE"
   ./scripts/scheduler/create_job.sh ./cases/autotune.csv "" $TAG AUTOTUNE
