7f3e5b83bca5cfd271b54f4a8f933d6651550ff9
Qiliang Cui <derrhein@gmail.com>
2025-07-03 18:51:41 +0000

    add jax autotune.
    Signed-off-by: Qiliang Cui <derrhein@gmail.com>

--- scripts/scheduler/hourly_run.sh
+++ scripts/scheduler/hourly_run.sh
@@ -29,8 +29,8 @@ echo "./scripts/scheduler/create_job.sh cases/hourly_torchaxspmd.csv \"\" $TAG H
 # Run TPU Commons + JAX test.
 # Eventually, JAX and vLLM should run the same test case.
 # for now, we start from v6e-1.
-echo "./scripts/scheduler/create_job.sh ./cases/hourly_jax.csv \"\" $TAG HOURLY_JAX TPU_COMMONS jax"
-# TODO: we ideally don't want to pin the vLLM version, but we want to keep it in sync with the TPU Commons-needed vLLM version
+# TODO(b/429439832): we ideally don't want to pin the vLLM version, but we want to keep it in sync with the TPU Commons-needed vLLM version
+echo "./scripts/scheduler/create_job.sh ./cases/hourly_jax.csv \"3c545c0c3\" $TAG HOURLY_JAX TPU_COMMONS jax"
 ./scripts/scheduler/create_job.sh ./cases/hourly_jax.csv "3c545c0c3" $TAG HOURLY_JAX TPU_COMMONS jax
 
 
@@ -46,6 +46,10 @@ if [[ "$HOUR_NOW" == "00" || "$HOUR_NOW" == "12" ]]; then
   # Torchax spmd
   echo "./scripts/scheduler/create_job.sh cases/autotune_torchaxspmd.csv \"\" $TAG AUTOTUNE_TORCHAX TPU_COMMONS" torchaxspmd
   ./scripts/scheduler/create_job.sh cases/autotune_torchaxspmd.csv "" $TAG AUTOTUNE_TORCHAX TPU_COMMONS torchaxspmd
+
+  # TODO(b/429439832): we ideally don't want to pin the vLLM version, but we want to keep it in sync with the TPU Commons-needed vLLM version
+  echo "./scripts/scheduler/create_job.sh ./cases/autotune_jax.csv \"3c545c0c3\" $TAG AUTOTUNE_JAX TPU_COMMONS jax"  
+  ./scripts/scheduler/create_job.sh ./cases/autotune_jax.csv "3c545c0c3" $TAG AUTOTUNE_JAX TPU_COMMONS jax
 
   # Adhoc
   echo "./scripts/scheduler/create_job.sh ./cases/autotune_adhoc.csv \"\" $TAG AUTOTUNE "
