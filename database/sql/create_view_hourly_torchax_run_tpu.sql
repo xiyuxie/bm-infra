5ada3ba90e3ba67630d594032db062f7e854936d
Qiliang Cui <derrhein@gmail.com>
2025-07-06 19:12:44 +0000

    Add mistralai/Mixtral-8x7B-Instruct-v0.1 in torchax run.
    Signed-off-by: Qiliang Cui <derrhein@gmail.com>

--- database/sql/create_view_hourly_torchax_run_tpu.sql
+++ database/sql/create_view_hourly_torchax_run_tpu.sql
@@ -7,6 +7,14 @@ SELECT
   RunRecord.CodeHash,
   RunRecord.Status,
   RunRecord.Device,
+  IFNULL(RunRecord.MedianITL, 0) AS MedianITL,
+  IFNULL(RunRecord.MedianTPOT, 0) AS MedianTPOT,
+  IFNULL(RunRecord.MedianTTFT, 0) AS MedianTTFT,
+  IFNULL(RunRecord.MedianETEL, 0) AS MedianETEL,
+  IFNULL(RunRecord.P99ITL, 0) AS P99ITL,
+  IFNULL(RunRecord.P99TPOT, 0) AS P99TPOT,
+  IFNULL(RunRecord.P99TTFT, 0) AS P99TTFT,
+  IFNULL(RunRecord.P99ETEL, 0) AS P99ETEL,
   IFNULL(RunRecord.Throughput, 0) AS Throughput,
   PARSE_TIMESTAMP('%Y%m%d_%H%M%S', RunRecord.JobReference, 'America/Los_Angeles') AS JobReferenceTime
 FROM
