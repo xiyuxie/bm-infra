33ce14ba4f84aac76e04f7da72bfa2c47e40c1ec
Qiliang Cui <derrhein@gmail.com>
2025-07-16 06:19:18 +0000

    Add view HourlyRunTorchaxJax.
    Signed-off-by: Qiliang Cui <derrhein@gmail.com>

---
+++ database/sql/create_view_hourly_run_torchax_jax_tpu.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE VIEW
+  `HourlyRunTorchaxJax` SQL SECURITY INVOKER AS
+SELECT
+  RunRecord.RecordId,
+  RunRecord.JobReference,
+  RunRecord.Model,
+  RunRecord.CodeHash,
+  RunRecord.Status,
+  RunRecord.Device,
+  IFNULL(RunRecord.MedianITL, 0) AS MedianITL,
+  IFNULL(RunRecord.MedianTPOT, 0) AS MedianTPOT,
+  IFNULL(RunRecord.MedianTTFT, 0) AS MedianTTFT,
+  IFNULL(RunRecord.MedianETEL, 0) AS MedianETEL,
+  IFNULL(RunRecord.P99ITL, 0) AS P99ITL,
+  IFNULL(RunRecord.P99TPOT, 0) AS P99TPOT,
+  IFNULL(RunRecord.P99TTFT, 0) AS P99TTFT,
+  IFNULL(RunRecord.P99ETEL, 0) AS P99ETEL,
+  IFNULL(RunRecord.Throughput, 0) AS Throughput,
+  PARSE_TIMESTAMP('%Y%m%d_%H%M%S', RunRecord.JobReference, 'America/Los_Angeles') AS JobReferenceTime
+FROM
+  RunRecord
+WHERE
+  RunRecord.RunType = 'HOURLY_AX_JAX'
+  AND RunRecord.Status IN ('COMPLETED',
+    'FAILED')
+  AND RunRecord.Device LIKE 'v6e-%'
+  AND RunRecord.CreatedTime >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 15 DAY)
+ORDER BY
+  RunRecord.JobReference;
