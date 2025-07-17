27d246b376a676576e9ed4fbe967ff279a03867a
Qiliang Cui <derrhein@gmail.com>
2025-07-17 23:33:29 +0000

    fix create config.
    Signed-off-by: Qiliang Cui <derrhein@gmail.com>

--- scripts/agent/create_config.sh
+++ scripts/agent/create_config.sh
@@ -25,7 +25,7 @@ ENV_FILE_TMP="$ENV_FILE.tmp"
 gcloud spanner databases execute-sql "$GCP_DATABASE_ID" \
   --instance="$GCP_INSTANCE_ID" \
   --project="$GCP_PROJECT_ID" \
-  --sql="SELECT RecordId, Model, CodeHash, MaxNumSeqs, MaxNumBatchedTokens, TensorParallelSize, MaxModelLen, Dataset, InputLen, OutputLen, ExpectedETEL, NumPrompts, ExtraEnvs FROM RunRecord WHERE RecordId = '$RECORD_ID';" | \
+  --sql="SELECT RecordId, Model, CodeHash, MaxNumSeqs, MaxNumBatchedTokens, TensorParallelSize, MaxModelLen, Dataset, InputLen, OutputLen, NumPrompts, ExpectedETEL, ExtraEnvs FROM RunRecord WHERE RecordId = '$RECORD_ID';" | \
   gawk 'NR==1 {
     for (i=1; i<=NF; i++) {
       # Insert underscore before uppercase letters preceded by a lowercase letter
@@ -35,9 +35,11 @@ gcloud spanner databases execute-sql "$GCP_DATABASE_ID" \
   }
   NR==2 {
     for (i=1; i<=NF; i++) {
-      print header[i] "=" $i
+      if ($i != "" && $i != "None") {
+        print header[i] "=" $i
+      }
     }
-  }'> $ENV_FILE_TMP
+  }' > "$ENV_FILE_TMP"
 
 # Process EXTRA_ENVS line
 EXTRA_ENVS_LINE=$(grep '^EXTRA_ENVS=' "$ENV_FILE_TMP")
