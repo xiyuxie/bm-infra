edf9d14b4b2eca612b48a4a8ef58d8fe29cf6ba5
Qiliang Cui <derrhein@gmail.com>
2025-07-23 21:03:34 +0000

    add a new region in cloud-tpu-inference-test.
    Signed-off-by: Qiliang Cui <derrhein@gmail.com>

---
+++ terraform/gcp/project1-2/variables.tf
@@ -0,0 +1,50 @@
+variable "project_id" {
+  default = "cloud-tpu-inference-test"
+}
+
+variable "region" {
+    default = "southamerica-west1"
+}
+
+variable "tpu_zone" {
+    default = "us-east5-b"
+}
+
+variable "purpose" {
+  default = "bm"
+}
+
+variable "spanner_instance" {
+  default = "vllm-bm-inst"
+}
+
+variable "spanner_db" {
+  default = "vllm-bm-runs"
+}
+
+variable "gcs_bucket" {
+  default = "vllm-cb-storage2"
+}
+
+variable "v6e_1_count" {
+  default     = 0
+}
+
+variable "v6e_4_count" {
+  default     = 1
+}
+
+variable "v6e_8_count" {
+  default     = 0
+}
+
+variable "instance_name_offset" {
+  type        = number
+  default     = 200
+  description = "instance name offset so that we can distinguish machines from different project or region."
+}
+
+variable "branch_hash" {
+  default     = "de69c2a5bfbdb186e44a88d0f6f8c2940b85261c"
+  description = "commit hash of bm-infra branch."
+}
