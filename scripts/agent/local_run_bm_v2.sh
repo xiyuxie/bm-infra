8c7fe26105d3655becbd4989aad29d26f0be9ca3
Qiliang Cui <derrhein@gmail.com>
2025-08-01 16:18:32 +0000

    debug uv env.
    Signed-off-by: Qiliang Cui <derrhein@gmail.com>

--- scripts/agent/local_run_bm_v2.sh
+++ scripts/agent/local_run_bm_v2.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 # enable uv
 export PATH="/home/bm-agent/.local/bin:$PATH"
 
+echo "================"
+echo "$PATH"
+which uv
+echo "================"
+
 if [ ! -f "$1" ]; then
   echo "Error: The env file '$1' does not exist."
   exit 1
